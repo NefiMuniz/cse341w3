6ffdc92bf3602ed713dbadfbcf539133186062b9
nefims <nefims@gmail.com>
2025-03-21 15:35:30 -0300

    Fixing render conecction

--- config/db.js
+++ config/db.js
@@ -3,7 +3,13 @@ require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(`${process.env.MONGODB_URL}/${process.env.DB_NAME}`);
+    const connectionString = `${process.env.MONGODB_URL}/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+    console.log('Connecting to MongoDB at:', connectionString);
+
+    await mongoose.connect(connectionString, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log('MongoDB connected successfully');
   } catch (error) {
     console.error('MongoDB connection error:', error.message);
