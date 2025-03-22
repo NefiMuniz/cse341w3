21c9767953e0c5338ad5a5e502ff3007734e3486
nefims <nefims@gmail.com>
2025-03-21 21:40:31 -0300

    Missionary Response Schema

--- swagger/swagger.js
+++ swagger/swagger.js
@@ -1,5 +1,6 @@
 const swaggerJsDoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
+const Missionary = require('../models/Missionary');
 
 const swaggerOptions = {
   swaggerDefinition: {
@@ -24,13 +25,15 @@ const swaggerOptions = {
         Missionary: {
           type: 'object',
           properties: {
-            firstName: { type: 'string' },
-            lastName: { type: 'string' },
-            whatsapp: { type: 'string' },
-            gender: { type: 'string', enum: ['M', 'F'] },
-            englishFluent: { type: 'string', enum: ['Yes', 'No'] },
-            missionStart: { type: 'string', format: 'date' },
-            missionEnd: { type: 'string', format: 'date' },
+            _id: { type: 'string', example: '67dcc0a5a396990a3760d6bc'},
+            firstName: { type: 'string', example: 'Nefi' },
+            lastName: { type: 'string', example: 'Muniz' },
+            whatsapp: { type: 'string', example: '+557799999999' },
+            gender: { type: 'string', enum: ['M', 'F'], example: 'M' },
+            englishFluent: { type: 'string', enum: ['Yes', 'No'], example: 'Yes' },
+            missionStart: { type: 'string', format: 'date', example: '01/01/2020' },
+            missionEnd: { type: 'string', format: 'date', example: '01/01/2022' },
+            pretitle: { type: 'string', enum: ['Elder', 'Sister'], example: 'Elder'},
           },
         },
         Class: {
@@ -43,6 +46,18 @@ const swaggerOptions = {
             assignedTo: { type: 'string', description: 'ID of the assigned missionary' },
           },
         },
+        MissionaryResponse: {
+          type: 'object',
+          properties: {
+            message: {
+              type: 'string',
+              example: 'Request fulfilled successfully!'
+            },
+            data: {
+              $ref: '#/components/schemas/Missionary'
+            }
+          }
+        }
       },
     },
   },
